252762c5196d10003805c689ad8d64d61092d213
Niroigen Suntharam <niroigen@gmail.com>
2018-08-22 00:57:45 -0400

    Adding capitalize to tester.cpp

--- src/tester.cpp
+++ src/tester.cpp
@@ -9,6 +9,6 @@ int main() {
 	print_hello_world();
 	std::cout << "3 + 4 = " << sum(3,4) << std::endl;
 	std::cout << "Niroigen reversed " << reverser("Niroigen") << std::endl;
-
+	std::cout << "taran capitalized " << capitalizer("taran") << std::endl;
 	return 0;
 }
