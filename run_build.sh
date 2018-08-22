cc5cded54f8d7918d07122b7617049df5ad1495c
Niroigen Suntharam <niroigen@gmail.com>
2018-08-22 16:46:12 -0400

    Adding gtest for proper unit testing

--- run_build.sh
+++ run_build.sh
@@ -11,29 +11,9 @@ showinfo() { echo -e "${BG}$1${NC}"; }
 workingprocess() { echo -e "${BB}$1${NC}"; }
 allert () { echo -e "${RED}$1${NC}"; }
 
-# Building project
-mkdir -p build
+cmake -H. -Bbuild
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j8
-# Checks if last comand didn't output 0
-# $? checks what last command outputed
-# If output is 0 then command is succesfuly executed
-# If command fails it outputs number between 0 to 255
-if ! make -j8
-then
-    error "Error: there are compile errors!"
-	# Terminate script and outputs 3
-    exit 3
-fi
-
-showinfo "Running tests ..."
-make -j8 Example_coverage
+cmake --build .
 ctest
-if ! ctest
-then
-    error "Error: there are failed tests!"
-    exit 4
-fi
 
 workingprocess "All tests compile and pass."
