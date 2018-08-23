3b4337e3f0367a15b6c9046c24160a60d0bc03ad
Niroigen Suntharam <niroigen@gmail.com>
2018-08-22 20:09:31 -0400

    Adding unit and build scripts

--- run_build.sh
+++ run_build.sh
@@ -1,18 +1,1 @@
-#! /bin/bash
-# Bash Script that builds project
-
-RED='\033[0;31m' # Red
-BB='\033[0;34m'  # Blue
-NC='\033[0m' # No Color
-BG='\033[0;32m' # Green
-
-error() { >&2 echo -e "${RED}$1${NC}"; }
-showinfo() { echo -e "${BG}$1${NC}"; }
-workingprocess() { echo -e "${BB}$1${NC}"; }
-allert () { echo -e "${RED}$1${NC}"; }
-
-cd build
-cmake --build .
-ctest
-
-set -e
+cmake -H. -Bbuild
